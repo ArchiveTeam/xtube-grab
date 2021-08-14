203a72f05336b29e954653d7597301d37dc66fef
arkiver <arkiver@protonmail.com>
2021-08-14 12:59:21 +0200

    Multi item size 100.

--- pipeline.py
+++ pipeline.py
@@ -54,11 +54,11 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20210813.02'
+VERSION = '20210813.03'
 USER_AGENT = 'Archive Team'
 TRACKER_ID = 'xtube'
 TRACKER_HOST = 'legacy-api.arpa.li'
-MULTI_ITEM_SIZE = 1
+MULTI_ITEM_SIZE = 100
 
 ###########################################################################
 # This section defines project-specific tasks.
